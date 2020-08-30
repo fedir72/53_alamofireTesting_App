1c03e0315e4ef396f2f8bf90d104f5e9966f52f0
fedir72 <fedir72@ua.fm>
2020-08-30 14:51:45 +0300

    start empty project

---
+++ 53_alamofireTest/53_alamofireTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  53_alamofireTest
+//
+//  Created by fedir on 30.08.2020.
+//  Copyright © 2020 fedir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
